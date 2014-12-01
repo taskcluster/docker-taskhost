87fd256fa272892c16e235bf6c2f200f814b221f
James Lal <james@lightsofapollo.com>
2014-12-01 11:26:15 -0800

    Bug 1106429 - Part 5 Add logs for each service.

--- test/images/server/server.js
+++ test/images/server/server.js
@@ -3,3 +3,5 @@ var http = require('http');
 http.createServer(function(req, res) {
   res.end(process.env.MAGIC_FOO || 'no magic');
 }).listen(80);
+
+console.log('Starting server', process.env.MAGIC_FOO);
--- test/integration/services_test.js
+++ test/integration/services_test.js
@@ -1,9 +1,28 @@
 suite('services', function() {
   var co = require('co');
   var testworker = require('../post_task');
+  var getArtifact = require('./helper/get_artifact');
+
+  test('schema validation', co(function* () {
+    var result = yield testworker({
+      payload: {
+        image: 'taskcluster/test-client',
+        command: [
+          '/bin/bash', '-c', 'curl amazingfoo'
+        ],
+        services: {
+          image: 'taskcluster/test-server',
+          env: { MAGIC_FOO: '$$foo$$' },
+          alias: 'amazingfoo'
+        },
+      }
+    });
+
+    assert.ok(result.log.indexOf('invalid json schema') !== -1, 'has log');
+    assert.ok(!result.run.success, 'task should be successful');
+  }));
 
   test('multiple http services', co(function* () {
-    var expected = 'is woot';
     var result = yield testworker({
       payload: {
         image: 'taskcluster/test-client',
@@ -27,8 +46,26 @@ suite('services', function() {
       }
     });
 
+    var artifacts = result.artifacts;
+
     assert.ok(result.log.indexOf('$$foo$$') !== -1);
     assert.ok(result.log.indexOf('$$bar$$') !== -1);
+    assert(artifacts['public/logs/services/amazingfoo.log']);
+    assert(artifacts['public/logs/services/amazingbar.log']);
     assert.ok(result.run.success, 'task should be successful');
+
+    console.log(artifacts);
+    var logs = yield {
+      amazingfoo: getArtifact(
+        result, artifacts['public/logs/services/amazingfoo.log'].name
+      ),
+
+      amazingbar: getArtifact(
+        result, artifacts['public/logs/services/amazingbar.log'].name
+      )
+    };
+
+    assert.ok(logs.amazingfoo.indexOf('$$foo$$') !== -1);
+    assert.ok(logs.amazingbar.indexOf('$$bar$$') !== -1);
   }));
 });
