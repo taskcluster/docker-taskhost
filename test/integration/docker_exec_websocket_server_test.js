d4089ae7eb76146ad96460e1f5a2d19c8c335a28
Edgar Chen <eggylv999@gmail.com>
2015-06-16 15:42:07 -0700

    beginning integration

--- test/integration/docker_exec_websocket_server_test.js
+++ test/integration/docker_exec_websocket_server_test.js
@@ -1,6 +1,11 @@
 var DockerWorker = require('../dockerworker');
 var TestWorker = require('../testworker');
 var assert = require('assert');
+var cmd = require('./helper/cmd');
+var debug = require('debug')('docker-worker:test:docker_exec_websocket_server_test')
+// var waitForEvent = require('../../lib/wait_for_event');
+var slugid = require('slugid');
+// var DockerExecClient = require('docker-exec-websocket-server').DockerExecClient;
 
 suite('use docker exec websocket server', () => {
   let worker;
@@ -15,7 +20,22 @@ suite('use docker exec websocket server', () => {
       worker = null;
     }
   });
-  test('cat', (done) => {
-  	done();
+  test('cat', async () => {
+  	let task = {
+      payload: {
+        image: 'taskcluster/test-ubuntu',
+        command: cmd('sleep 30'),
+        maxRunTime: 40,
+        features: {
+          websocketServer: true
+        }
+      }
+    };
+
+    let result = await worker.postToQueue(task);
+    // var client = new DockerExecClient({
+
+    // })
+    debug(result);
   });
  });
