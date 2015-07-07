a82a6bf145c4c67d32bbc2d37470635f8f903323
Greg Arndt <garndt@mozilla.com>
2015-07-07 13:45:36 -0500

    Add test for pulling images by sha

--- test/integration/pull_image_test.js
+++ test/integration/pull_image_test.js
@@ -7,12 +7,27 @@ suite('pull image', function() {
   var cmd = require('./helper/cmd');
 
   var IMAGE = 'ubuntu:12.10';
+  const SHA_IMAGE = 'ubuntu@sha256:e2ff67c8f9bb7e1eda3ec573fd8ac74b976d170d9e13babc5c5035f4443af744';
 
   test('ensure image can be pulled', co(function* () {
     yield dockerUtils.removeImageIfExists(docker, IMAGE);
     var result = yield testworker({
       payload: {
         image: IMAGE,
+        command: cmd('ls'),
+        maxRunTime: 5 * 60
+      }
+    });
+
+    assert.equal(result.run.state, 'completed', 'task should be successfull');
+    assert.equal(result.run.reasonResolved, 'completed', 'task should be successfull');
+  }));
+
+  test('ensure image can be pulled using sha hash', co(function* () {
+    yield dockerUtils.removeImageIfExists(docker, SHA_IMAGE);
+    var result = yield testworker({
+      payload: {
+        image: SHA_IMAGE,
         command: cmd('ls'),
         maxRunTime: 5 * 60
       }
