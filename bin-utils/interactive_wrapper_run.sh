ebc42728bff810d891fa67ca1fb02e4f5e21a8ef
Edgar Chen <eggylv999@gmail.com>
2015-07-08 16:34:43 -0700

    line endings

--- bin-utils/interactive_wrapper_run.sh
+++ bin-utils/interactive_wrapper_run.sh
@@ -8,6 +8,7 @@ wait $PID
 trap - TERM INT
 wait $PID
 EXIT_STATUS=$?
+# make sure it doesn't die before the interactive gets a hold of it
 sleep 5
 flock -x /tmp/interactive.lock true
 exit $EXIT_STATUS
