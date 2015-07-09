889ceb1b087dcb43ad226c5703c97cc218df740d
Edgar Chen <eggylv999@gmail.com>
2015-07-09 16:44:45 -0700

    adding debug messages

--- bin-utils/interactive_wrapper_run.sh
+++ bin-utils/interactive_wrapper_run.sh
@@ -8,7 +8,7 @@ wait $PID
 trap - TERM INT
 wait $PID
 EXIT_STATUS=$?
-# make sure it doesn't die before the interactive gets a hold of it
+# make sure it doesn't die before the interactive feature's file lock locks it
 sleep 5
 flock -x /.taskclusterinteractivesession.lock true
 exit $EXIT_STATUS
