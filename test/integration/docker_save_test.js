59593db0327e2bbb60307f7147f972a26a18dc4d
Edgar Chen <eggylv999@gmail.com>
2015-07-15 12:56:06 -0700

    wip

--- test/integration/docker_save_test.js
+++ test/integration/docker_save_test.js
@@ -1,4 +1,5 @@
 import assert from 'assert';
+import base from 'taskcluster-base'
 import Debug from 'debug';
 import Docker from 'dockerode-promise';
 import DockerWorker from '../dockerworker';
@@ -6,6 +7,7 @@ import fs from 'mz/fs';
 import https from 'https';
 import request from 'superagent-promise';
 import TestWorker from '../testworker';
+import zlib from 'zlib';
 
 let debug = Debug('docker-worker:test:docker-save-test');
 
@@ -51,37 +53,37 @@ suite('use docker-save', () => {
     //superagent was only downlading 16K of data
     //TODO: work on error handling here
     await new Promise((accept, reject) => {
-      https.request(signedUrl, (res) => {
+      https.request(signedUrl, (res) => { //take the redirect
         https.request(res.headers.location, (res) => {
-          res.pipe(fs.createWriteStream('/tmp/dockerload.tar'));
-          res.on('end', accept);
+          let unzipStream = zlib.Gunzip();
+          res.pipe(unzipStream).pipe(fs.createWriteStream('/tmp/dockerload.tar'));
+          unzipStream.on('end', accept);
+          res.on('error', (err) => reject(err));
         }).end();
+        res.on('error', (err) => reject(err));
       }).end();
     });
 
-    
-
-    // let res = await request.get(signedUrl).end();
-    // debug(res.statusCode);
-    // await new Promise((accept, reject) => {
-    //     res.pipe(fs.createWriteStream('/tmp/dockerload.tar'));
-    //     res.on('end', accept);
-    //     res.on('data', (dat) => {
-    //       debug('%s bytes recieved', dat.length);
-    //     });
-    // });
-
-    // //maybe there's a better way to get the docker obj than making a new one
-    // let docker = new Docker();
-    // await docker.load('/tmp/dockerload.tar');
-    // let opts = {
-    //   AttachStdout: true,
-    //   AttachStderr: true,
-    //   Cmd: ['cat', '/tmp/test.log'],
-    //   Image: newImg.name
-    // }
-    // let container = await docker.createContainer(opts);
-    // debug(container);
+    //maybe there's a better way to get the docker obj than making a new one
+    let docker = new Docker();
+    await docker.loadImage('/tmp/dockerload.tar');
+    let opts = {
+      AttachStdout: true,
+      AttachStderr: true,
+      Cmd: ['cat', '/tmp/test.log'],
+      Image: taskId + '/' + runId
+    };
+    let streamOpts = {
+      stdout: true,
+      stream: true,
+    }
+    let container = await docker.createContainer(opts);
+    await container.start();
+    let stream = container.attach(streamOpts);
+    stream.on('data', (data) => {
+      debug(data);
+    });
+    await base.testing.sleep(10000);
   });
 });
 
