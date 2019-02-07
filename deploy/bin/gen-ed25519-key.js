c61ca48148bc18c5e962f7f53b7e674c3d5fb20f
Aki Sasaki <aki@escapewindow.com>
2019-02-07 10:02:18 -0800

    writeFileSync for creating the ed25519 pubkey

--- deploy/bin/gen-ed25519-key.js
+++ deploy/bin/gen-ed25519-key.js
@@ -7,7 +7,7 @@ function main() {
   console.log('Generating ed25519 keypair...');
   let keypair = tweetnacl.sign.keyPair();
 
-  fs.writeFile('docker-worker-ed25519.pub', new Buffer.from(keypair.publicKey).toString('base64'), function(err) {});
+  fs.writeFileSync('docker-worker-ed25519.pub', new Buffer.from(keypair.publicKey).toString('base64'), function(err) {});
   return fs.writeFileSync('docker-worker-ed25519-cot-signing-key.key', new Buffer.from(keypair.secretKey).toString('base64'), function(err) {});
 }
 
